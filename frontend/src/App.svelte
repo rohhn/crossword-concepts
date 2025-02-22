1679e7e63fe23f0e871a0529898b1339e667f76e
dunkeln <prateekpravanjan@gmail.com>
2025-02-22 11:36:46 -0500

    frontend init

---
+++ frontend/src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import "./app.css";
+  import { Toaster } from "$lib/components/ui/sonner";
+  import { toast } from "svelte-sonner";
+  import { Button } from "$lib/components/ui/button";
+</script>
+
+<main>
+  <Button variant="outline" on:click={() => toast.success("site works")}>
+    Invoke
+  </Button>
+</main>
+
+<Toaster />
