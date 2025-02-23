d661c2e55e5487c27aed39484590d024bbe96bee
dunkeln <prateekpravanjan@gmail.com>
2025-02-23 14:55:52 -0500

    minor updates bfore backend work

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,8 +2,10 @@
   import "./app.css";
   import { Link, Router, Route } from "svelte-routing";
   import { Toaster } from "$lib/components/ui/sonner";
+  import { User, Puzzle } from "lucide-svelte";
   import Content from "./routes/Content.svelte";
   import Auth from "./routes/Auth.svelte";
+  import About from "./routes/About.svelte";
   let url = "";
 </script>
 
@@ -15,16 +17,16 @@
       class="flex justify-between items-center w-[75vw] p-0 rounded-md font-mono"
     >
       <h5 class="font-semibold font-mono">crossword concepts</h5>
-      <nav class="flex flex-row-reverse">
+      <nav class="flex flex-row pb-[1rem] text-md">
         <Link to="/" class="m-[.4rem]">content</Link>
-        <Link to="/about" class="m-[.4rem]">content</Link>
-        <Link to="/auth" class="m-[.4rem]">auth</Link>
+        <Link to="/about" class="m-[.4rem]">about</Link>
+        <Link to="/auth" class="m-[.4rem]"><User size="1.5rem" /></Link>
       </nav>
     </div>
     <div>
       <Route path="/auth" component={Auth} />
       <Route path="/" component={Content} />
-      <Route path="/about" component={Content} />
+      <Route path="/about" component={About} />
     </div>
   </Router>
 </main>
---
+++ frontend/src/routes/About.svelte
@@ -0,0 +1,1 @@
+<div class="w-[75vw] h-[90vh]">this is the about page</div>
