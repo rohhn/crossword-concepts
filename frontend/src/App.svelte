997ba4f06cd0a76750d8d99d156f7aee54f74cce
dunkeln <prateekpravanjan@gmail.com>
2025-02-22 22:26:01 -0500

    puzzle component start

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3,12 +3,14 @@
   import { Toaster } from "$lib/components/ui/sonner";
   import { toast } from "svelte-sonner";
   import { Button } from "$lib/components/ui/button";
+  import Crossword from "$lib/components/crossword/Crossword.svelte";
 </script>
 
 <main>
   <Button variant="outline" on:click={() => toast.success("site works")}>
     Invoke
   </Button>
+  <Crossword />
 </main>
 
 <Toaster />
